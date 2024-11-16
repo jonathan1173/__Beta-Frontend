5579bf68d82ca1174930ad1ed37408e92d23d4b1
Jonathan <Jonathan106e@gmail.com>
2024-11-16 15:54:21 -0600

    corriguiendo el error de los filtros menu

--- src/components/FiltersMenu.js
+++ src/components/FiltersMenu.js
@@ -2,12 +2,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const Filters = ({
+const FiltersMenu = ({
     handleCategoryClick,
     handleLanguageClick,
     handleDifficultyClick,
-    handleFavoriteClick,
-    handleMostLikesClick
+    
 }) => {
     const [categories, setCategories] = useState([]);
     const [languages, setLanguages] = useState([]);
@@ -15,12 +14,11 @@ const Filters = ({
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
-    // Cargar las opciones de filtros desde la API
     useEffect(() => {
         const fetchFilterOptions = async () => {
             try {
                 setLoading(true);
-                setError(null);  // Resetear el error antes de la nueva solicitud
+                setError(null);  
                 const token = localStorage.getItem('access_token');
                 const response = await axios.get(`http://localhost:8000/beta/challenges/filters/`, {
                     headers: { Authorization: `Bearer ${token}` },
@@ -83,27 +81,8 @@ const Filters = ({
                     </option>
                 ))}
             </select>
-
-{/* sin funcionar  */}
-            {/* Filtro de Favoritos */}
-            <select
-                onChange={(e) => handleFavoriteClick(e.target.value)}
-                className="border border-gray-300 p-2 rounded"
-            >
-                <option value="">Todos los Desafíos</option>
-                <option value="true">Solo Favoritos</option>
-                <option value="false">Excluir Favoritos</option>
-            </select>
-            {/* Filtro de Mayoría de Likes */}
-            <select
-                onChange={(e) => handleMostLikesClick(e.target.value)}
-                className="border border-gray-300 p-2 rounded"
-            >
-                <option value="">Todos los Desafíos</option>
-                <option value="true">Mayoría de Likes</option>
-            </select>
         </div>
     );
 };
 
-export default Filters;
+export default FiltersMenu;
